db9112c2d25e5343b8d68f94d1262c88a2d0ca6c
Josep Boldu <jbolduqu8@alumnes.ub.edu>
2015-08-19 14:09:25 +0200

    connexio i inici de recopilacio de dades

--- catastro/catastro.py
+++ catastro/catastro.py
@@ -1,23 +1,44 @@
 # -*- coding: cp1252 -*-
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-import string, csv, time
+import string, csv, time,urllib, urllib2
+from bs4 import BeautifulSoup
 
+referencia = "0990511VL5009S0030AL"
 #print "Introduce la Referencia Catastral a buscar:"
 #referencia = input()
-referencia = "0990511VL5009S0030AL"
 
-wd = webdriver.Firefox()
-
-wd.get("https://www1.sedecatastro.gob.es/OVCFrames.aspx?TIPO=Consulta")
-
+driver = webdriver.Firefox()
 driver.maximize_window()
-driver.implicitly_wait(20)
+driver.get("https://www1.sedecatastro.gob.es/OVCFrames.aspx?TIPO=Consulta")
+driver.switch_to_default_content()
+driver.switch_to_frame("registrado") 
+driver.find_element_by_id("txtRC").send_keys(referencia)
+driver.find_element_by_id("btnAlfanumerico").click()
 
-wd.find_element_by_id("txtRC.textogris").send_keys(referencia)
+########################Conexion y consulta####################
 
-#wd.find_element_by_id("btnAlfanumerico").click()
+html = driver.page_source
+soup = BeautifulSoup(html)
+print "----Datos del Bien Inmueble----"
+temp = soup.find("table", { "id" : "ctl00_body_tblInmueble" })
 
+temp = temp.findAll("tr")
+del temp[0]
+i=0
+aux=[]
+for item in temp:
+    temp2 = item.findAll("td")
+    if i == 1:
+        aux.append(temp2[1].find("span").contents)
+    else:
+        aux.append(temp2[1].find("span").contents[0])
+    i+=1
 
+print aux
+
+print "----Datos de la Finca en la que se integra el Bien Inmueble----"
+temp = soup.find("table", { "id" : "ctl00_body_tblFinca" })
+temp = temp.findAll("tr")
+del temp[0]
 print "------------------------FINAL------------------------"
